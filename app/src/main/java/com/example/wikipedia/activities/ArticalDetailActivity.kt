f27121344db36b528bbbec7df7cb008866f2348d
macxwhale <macxwhale@gmail.com>
2019-07-19 11:54:00 +0300

    "Wikipedia WIth Kotlin"

---
+++ app/src/main/java/com/example/wikipedia/activities/ArticalDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wikipedia.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import com.example.wikipedia.R
+
+class ArticalDetailActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
---
+++ app/src/main/java/com/example/wikipedia/activities/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.wikipedia.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.example.wikipedia.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+
+        message.setOnClickListener {
+            startActivity(Intent(this,ArticalDetailActivity::class.java))
+        }
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
